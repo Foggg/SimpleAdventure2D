36434538a3200b8ec3364787c89c1fddeb780711
Kirill Smirnov <kirils.smirnovs@28stone.com>
2020-07-16 13:55:39 +0300

    Initial commit

---
+++ desktop/src/com/fog1984/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fog1984.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fog1984.game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
