b6614edd65e856ffb08dba85c85ccd7a054d8ab6
Kirill Smirnov <kirils.smirnovs@28stone.com>
2020-07-16 18:07:37 +0300

    Camera is added. GameScreen is updated

--- core/src/com/fog1984/game/views/GameScreen.java
+++ core/src/com/fog1984/game/views/GameScreen.java
@@ -5,32 +5,47 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameScreen implements Screen {
+public class GameScreen extends MainScreen implements Screen {
 
-    private final Game game;
     private Texture gameImg;
-    private final MainScreen mainScreen;
+    private OrthographicCamera camera;
 
-    public GameScreen(Game game, MainScreen mainScreen){
-        this.game = game;
-        this.mainScreen = mainScreen;
+    float x = 512;
+
+    public GameScreen(Game game) {
+        super(game);
     }
 
     @Override
     public void show() {
+        batch = new SpriteBatch();
         gameImg = new Texture("game.jpg");
+        gameImg.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(512, 384, 0);
+        sprite = new Sprite(new Texture(Gdx.files.internal("chuk.png")));
+        sprite.setBounds(0, 0, 250,500);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        mainScreen.batch.begin();
-        mainScreen.batch.draw(gameImg, 0,0);
-        mainScreen.batch.end();
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(gameImg, 0, 0, 2048, 1536);
+        batch.draw(sprite, 1500,100 , 50,100);
+        x++;
+        camera.position.x = x;
+        batch.end();
 
         if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
+            pause();
             game.setScreen(new MainScreen(game));
         }
     }
@@ -42,7 +57,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void pause() {
-
     }
 
     @Override
@@ -57,7 +71,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        mainScreen.dispose();
         gameImg.dispose();
         game.dispose();
     }
--- core/src/com/fog1984/game/views/MainScreen.java
+++ core/src/com/fog1984/game/views/MainScreen.java
@@ -7,15 +7,17 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainScreen implements Screen {
 
     public SpriteBatch batch;
-    private final Game game;
+    public Game game;
     private Texture mainTexture;
     private BitmapFont gameName;
     private BitmapFont pressEnter;
+    public Sprite sprite;
 
     public MainScreen(Game game) {
         this.game = game;
@@ -25,23 +27,27 @@ public class MainScreen implements Screen {
     public void show() {
         batch = new SpriteBatch();
         mainTexture = new Texture("cover.jpg");
+        mainTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         gameName = new BitmapFont();
         pressEnter = new BitmapFont();
+        sprite = new Sprite(mainTexture);
+        sprite.setBounds(200, 200, 500,250);
+
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(mainTexture, 175, 200);
-        gameName.draw(batch,"Simple Adventure 2D", 700,700);
-        pressEnter.draw(batch,"Press Enter to Start", 200,200);
-        gameName.setColor(1,1,0,1);
-        pressEnter.setColor(1,0,0,1);
+        sprite.draw(batch);
+        gameName.draw(batch, "Simple Adventure 2D", 700, 700);
+        pressEnter.draw(batch, "Press Enter to Start", 200, 200);
+        gameName.setColor(1, 1, 0, 1);
+        pressEnter.setColor(1, 0, 0, 1);
         batch.end();
 
         if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
-            game.setScreen(new GameScreen(game, this));
+            game.setScreen(new GameScreen(game));
         }
     }
 
